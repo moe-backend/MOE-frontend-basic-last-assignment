3a7b6bb3b04cb0d40800f2bc279390cc51275430
Moe Katakura <m.katakura@4nonome.com>
2022-08-04 16:16:27 +0900

    first-commit

---
+++ src/stories/organisms-about.stories.js
@@ -0,0 +1,6 @@
+import Item_About from "../components/organisms/about/about.html";
+import "../components/organisms/about/about.css";
+
+export default { title: "Organisms/About" };
+
+export const About = () => Item_About;
